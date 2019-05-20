d5ce6f80bb9c1f61beebd6aad0105ce0be7d9b08
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-05-20 13:35:38 +0430

    Costumer users mock data added

---
+++ src/mockData/costumers.js
@@ -0,0 +1,32 @@
+export const costumers = [
+    {
+        id:1,
+        fullName:'نیما کمالی',
+        city:1,
+        status:true
+    },
+    {
+        id:2,
+        fullName:'سجاد رضایی',
+        city:2,
+        status:true
+    },
+    {
+        id:3,
+        fullName:'شهریار فدایی',
+        city:4,
+        status:false
+    },
+    {
+        id:4,
+        fullName:'رحیم قندی',
+        city:3,
+        status:true
+    },
+    {
+        id:5,
+        fullName:'شاهرخ سلامت',
+        city:5,
+        status:false
+    },
+];
