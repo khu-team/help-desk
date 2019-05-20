a729ec7cf4e1af78be1f810467e16336013f5c5c
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-05-20 13:31:58 +0430

    Support team users mock data added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './supportTeam.css';
+import './tickets.css';
+import './products.css';
+import './costumers.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const jsx = (
+    <div>
+        <App/>
+    </div>
+);
+
+
+ReactDOM.render(jsx, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
