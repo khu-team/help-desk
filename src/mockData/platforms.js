df1e879fdf1d6ca2e66b304fd9a509b7a00581e2
Hossein Ahmadi <hossein.ahmadi98@outlook.com>
2019-05-20 13:49:19 +0430

    Products  mock data added

---
+++ src/mockData/platforms.js
@@ -0,0 +1,10 @@
+export const platforms = [
+    {
+        id: 1,
+        name: 'Android'
+    },
+    {
+        id: 2,
+        name: 'iOS'
+    }
+];
---
+++ src/mockData/serverTypes.js
@@ -0,0 +1,10 @@
+export const serverTypes = [
+    {
+        id:1,
+        name:'هاست'
+    },
+    {
+        id:2,
+        name:'سرور'
+    }
+];
