364a61bf339699f8a7f6c26382d4beb09a595820
alirezaborzuei <45588535+alirezaborzuei@users.noreply.github.com>
2019-05-22 13:42:55 +0430

    add file to my branch

---
+++ src/component/Costumers/CostumersList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CostumersList = () => (
+    <div className="welcome">
+        Start From Here...
+    </div>
+);
+
+export default CostumersList;
---
+++ src/mockData/supportTeamUsers.js
@@ -0,0 +1,38 @@
+export const supportTeamUsers = [
+    {
+        id:1,
+        fullName:'محمد ریاحی',
+        role:1,
+        department:1,
+        email:'email@domain.com',
+        phoneNumber:'09121234567',
+        status:true
+    },
+    {
+        id:2,
+        fullName:'شیلا محمدیان',
+        role:2,
+        department:1,
+        email:'email@domain.com',
+        phoneNumber:'09121234567',
+        status:true
+    },
+    {
+        id:3,
+        fullName:'خشایار صادقی',
+        role:1,
+        department:2,
+        email:'email@domain.com',
+        phoneNumber:'09121234567',
+        status:true
+    },
+    {
+        id:4,
+        fullName:'سهراب نادری',
+        role:2,
+        department:3,
+        email:'email@domain.com',
+        phoneNumber:'09121234567',
+        status:true
+    },
+];
