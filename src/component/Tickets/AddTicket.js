cecd4dbf9e70e50dd31b700fafc23c74577608dd
jasmine-ftkh <i.yasaman.eftekhari@gmail.com>
2019-05-22 16:05:40 +0430

    added basic form structure

--- src/component/Tickets/AddTicket.js
+++ src/component/Tickets/AddTicket.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React from "react";
+import { products } from "../../mockData/products";
+import { departments } from "../../mockData/departments";
 
-const AddTicket = () => (
-    <div className="welcome">
-        Start From Here...
-    </div>
-);
-
+class AddTicket extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="addTicket">
+          {/* input for title  */}
+          <input placeholder="عنوان" type="text" name="title" />
+          {/* dropdown for product */}
+          <select name="product">
+            {products.map(product => (
+              <option key={product.id} value={product.id}>
+                {product.name}
+              </option>
+            ))}
+          </select>
+          {/* dropdown for department */}
+          <select name="department">
+            {departments.map(department => (
+              <option key={department.id} value={department.id}>
+                {department.name}
+              </option>
+            ))}
+          </select>
+          {/* dropdown for priority */}
+          <label htmlFor="priority">Priority</label>
+          <select name="priority" id="priority" >
+            <option value="HIGH">High</option>
+            <option value="MEDIUM">Medium</option>
+            <option value="LOW">Low</option>
+          </select>
+          {/* textArea for description */}
+          <textarea
+            placeholder="توضیحات"
+            name="description"
+            cols="30"
+            rows="10"
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
 export default AddTicket;
