96cc6ece1c1d2e63aca0bb26301de03622f77be7
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2019-05-23 21:21:15 +0430

    Update SupportTeamItem.js

--- src/component/SupportTeam/SupportTeamItem.js
+++ src/component/SupportTeam/SupportTeamItem.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-
-export const SupportTeamItem = () => (
+export const SupportTeamItem = ({fullName,email,phoneNumber,status,department,role}) => (
     <div >
+        <table>
+            <tbody>
+            <tr>
 
-        
+            </tr>
+
+            </tbody>
+        </table>
     </div>
 );
