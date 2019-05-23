2d5f4be738da7a416eafbee3a3b1f949514738bf
aria-23 <ariakhodadadeh78@gmail.com>
2019-05-23 15:36:58 +0430

    fixed a bug

---
+++ src/component/Tickets/DepartmentRenderer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { departments } from '../../mockData/departments';
+
+const DepartmentRenderer = (props) => {
+	const department = (departments.find((department) => department.id === props.departmentId));
+	return (<td>{department.name}</td>);
+}
+
+export default DepartmentRenderer;
