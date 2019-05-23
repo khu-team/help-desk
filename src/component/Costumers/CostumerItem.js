49df7c4e06c3ea169faaf2adb35673d09d01cfe4
alirezaborzuei <45588535+alirezaborzuei@users.noreply.github.com>
2019-05-23 16:43:05 +0430

    I delete my costumer component and add new file include of city render,costumerItem,Costumerlist,statusCostumer,costumer.css file that all of them write by myself

---
+++ src/component/Costumers/CostumerItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {cityRenderer} from '../../component/Costumers/City Render'
+import {Status} from '../../component/Costumers/Status Costumer'
+export const CostumerItem = ({ fullName, city, id,status}) => (
+    <div > 
+     <table class="GeneratedTable">
+  <tbody>
+    <tr>
+    
+      <th>{Status(status,id)}&nbsp;&nbsp;&nbsp;&nbsp;</th>
+      <th>{cityRenderer(city, id)}&nbsp;&nbsp;&nbsp;&nbsp;</th>
+      <th>{fullName} &nbsp;&nbsp;&nbsp;&nbsp;</th>
+      
+     
+    </tr>
+   
+  </tbody>
+</table>
+
+
+
+
+    </div>
+    );
