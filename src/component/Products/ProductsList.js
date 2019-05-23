aad5c18c43ee444488733f53f1637882263f860c
alirezaborzuei <45588535+alirezaborzuei@users.noreply.github.com>
2019-05-24 02:24:20 +0430

    add main task to do that miss barbari changed. this main and my team please dont change it

---
+++ src/component/Products/ProductsList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductsList = () => (
+    <div className="welcome">
+        Start From Here...
+    </div>
+);
+
+export default ProductsList;
---
+++ src/component/SupportTeam/SupportTeamList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SupportTeamList = () => (
+    <div className="welcome">
+        Start From Here...
+    </div>
+);
+
+export default SupportTeamList;
