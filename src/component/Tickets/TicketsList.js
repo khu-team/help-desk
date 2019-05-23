eb11d26d3651517acee25fb2bfcd490acafa5233
mohammad <ma_kazemi@yahoo.com>
2019-05-23 14:01:44 +0430

    TicketsList done

--- src/component/Tickets/TicketsList.js
+++ src/component/Tickets/TicketsList.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import {tickets} from '../../mockData/tickets';
+import TicketItem from './TicketItem';
 
 const TicketsList = () => (
-    <div className="welcome">
-        Start From Here...
-    </div>
+	<React.Fragment>
+		<div>
+			<table border="2" cellPadding="10">
+				<tr>
+					<th>ID</th>
+					<th>Title</th>
+					<th>Priority</th>
+					<th>Costumer</th>
+					<th>Product</th>
+					<th>Department</th>
+					<th>AnswerStatus</th>
+					<th>SubmissionDateTime</th>
+				</tr>
+				{tickets.map((item) => (<TicketItem ticket={item} key={item.id} />))}
+			</table>
+		</div>
+	</React.Fragment>
 );
 
 export default TicketsList;
