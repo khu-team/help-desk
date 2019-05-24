f32fa617462bb0122e201f8ea8f2393dddbcd0c8
MehradNV <mehradnoorani@yahoo.com>
2019-05-24 18:52:29 +0430

    Revert "improve productitem and some changes on table"
    This reverts commit 3909a764afac3333675b7574949a23b36cc0de08.

--- src/component/Products/ProductItem.js
+++ src/component/Products/ProductItem.js
@@ -3,35 +3,17 @@ import ProductTable from "./ProductTable";
 
 class ProductItem extends React.Component{
 
-    renderTableData() {
-        var numeral = require('numeral');
-       
-        return this.props.data.map((product,index) => {
-           
-            const num = numeral(product.price).format('0,0');
-            const persent= product.discountPercentage/100;
-            const nprice=product.price -persent*product.price;
-            const newprice=numeral(nprice).format('0,0');
-            const { id, name,  discountPercentage } = product ;
-            
-            return (
-                <div>
 
-               
-                    <tr key={id}>
+    renderTableData() {
+        return this.props.data.map((product,index) => {
+            const { id, name, price , discountPercentage } = product ;
+            return (
+                <tr key={id}>
                     <td>{id}</td>
                     <td>{name}</td>
-                    <td>{num}__</td>
-                    <td>{discountPercentage}___</td>
-                    <td>{newprice}</td>
-
-
-
+                    <td>{price}</td>
+                    <td>{discountPercentage}</td>
                 </tr>
-
-
-                </div>
-               
             )
         })
     }
