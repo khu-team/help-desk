dedcb593e0f576161e45f9b88a86ad2f2626f4ea
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2019-05-25 01:46:19 +0430

    Update DepartmentRenderer.js

--- src/component/SupportTeam/DepartmentRenderer.js
+++ src/component/SupportTeam/DepartmentRenderer.js
@@ -1,27 +1,7 @@
-import React, { Component } from "react";
-import departments, { id, name } from "./mockdata/departments";
+import React from "react";
+import {departments} from "../../mockData/departments";
 
-class departmentss extends Component {
-  state = {};
-  constructor(props) {
-    super(props);
-  }
-
-  getdepartmentById(id) {
-    return (
-      <span>
-        {" "}
-        {
-          departments.find(function(element) {
-            return element.id == id;
-          }).name
-        }
-      </span>
-    );
-  }
-  render() {
-    return this.getdepartmentById(this.props.id);
-  }
+export const DepartmentRenderer = (department, id) => {
+  const FindDepartment = departments.find((item) => item.id === department);
+  return <span  key={id}>{FindDepartment.name}</span>
 }
-
-export default departmentss;
