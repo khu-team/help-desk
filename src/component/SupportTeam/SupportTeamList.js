beb6d27bd4a6bdf9f8a190c651132e5fadfa94c2
abdullahyazge <45951464+abdullahyazge@users.noreply.github.com>
2019-05-24 13:38:24 -0700

    Update SupportTeamList.js

--- src/component/SupportTeam/SupportTeamList.js
+++ src/component/SupportTeam/SupportTeamList.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {SupportTeamItem} from '../../component/SupportTeam/SupportTeamItem'
+import { supportTeamUsers } from '../../mockData/supportTeamUsers'
+
 
 const SupportTeamList = () => (
     <div className="welcome">
-        Start From Here...
+        <table >
+            <thead>
+            <tr>
+                <th> وضعیت</th>
+                <th> تلفن تماس</th>
+                <th> ایمیل</th>
+                <th> دپارتمان</th>
+                <th>نقش</th>
+                <th>نام کامل</th>
+
+            </tr>
+            </thead>
+        </table>
+        {supportTeamUsers.map((supporters) => (
+            <SupportTeamItem  {...supporters} />
+
+        ))}
     </div>
 );
 
-export default SupportTeamList;
+export default SupportTeamList ;
