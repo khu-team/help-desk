3909a764afac3333675b7574949a23b36cc0de08
maryam-Si <49492170+maryam-Si@users.noreply.github.com>
2019-05-24 18:10:35 +0430

    improve productitem and some changes on table

--- src/component/Products/ProductItem.js
+++ src/component/Products/ProductItem.js
@@ -3,17 +3,35 @@ import ProductTable from "./ProductTable";
 
 class ProductItem extends React.Component{
 
-
     renderTableData() {
+        var numeral = require('numeral');
+       
         return this.props.data.map((product,index) => {
-            const { id, name, price , discountPercentage } = product ;
+           
+            const num = numeral(product.price).format('0,0');
+            const persent= product.discountPercentage/100;
+            const nprice=product.price -persent*product.price;
+            const newprice=numeral(nprice).format('0,0');
+            const { id, name,  discountPercentage } = product ;
+            
             return (
-                <tr key={id}>
+                <div>
+
+               
+                    <tr key={id}>
                     <td>{id}</td>
                     <td>{name}</td>
-                    <td>{price}</td>
-                    <td>{discountPercentage}</td>
+                    <td>{num}__</td>
+                    <td>{discountPercentage}___</td>
+                    <td>{newprice}</td>
+
+
+
                 </tr>
+
+
+                </div>
+               
             )
         })
     }
--- src/component/Products/ProductTable.js
+++ src/component/Products/ProductTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProductItem from "./ProductItem";
-
+import './style.css';
 
 class ProductTable extends React.Component{
 
@@ -9,15 +9,16 @@ class ProductTable extends React.Component{
         return (
             <div>
                 <h1 id='title'></h1>
-                <table id='products'>
-                    <tbody>
+                <table id="customers">
+                    
                     <tr key={6}>
                         <th>ID</th>
                         <th>نام محصول</th>
                         <th>قیمت</th>
                         <th>درصد تخفیف</th>
+                        <th>قیمت با تخفیف</th>
                     </tr>
-                    </tbody>
+                    
                 </table>
             </div>
         )
