6ad51d092981b3a47fb7bab9de2fe13b685f8d7b
sinamoraddar <sinamoraddar@gmail.com>
2019-05-25 13:03:18 +0430

    added some resopnsive stylings

--- src/component/Tickets/AddTicket.js
+++ src/component/Tickets/AddTicket.js
@@ -95,40 +95,50 @@ class AddTicket extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {this.state.titleError && <p className="addTicket__message addTicket__message--error">{this.state.titleError}</p>}
-        {this.state.descriptionError && <p className="addTicket__message addTicket__message--error">{this.state.descriptionError}</p>}
         <form className="addTicket" onSubmit={(event) => this.onSubmit(event)}>
+          {this.state.titleError && <p className="addTicket__message addTicket__message--error">{this.state.titleError}</p>}
+          {this.state.descriptionError && <p className="addTicket__message addTicket__message--error">{this.state.descriptionError}</p>}
           {/* input for title  */}
-          <div className="label">
-            <label htmlFor="title">عنوان :  </label></div>
-          <input placeholder="عنوان" type="text" name="title" value={this.state.title} onChange={(event) => this.onFormChange(event)} />
-          <br></br>{/* dropdown for product */}
-          <label htmlFor="product">محصول :  </label>
-          <select name="product" id="product" value={this.state.product} onChange={(event) => this.onFormChange(event)} >
-            {
-              products.map((product) => <option key={product.id} value={product.id}>{product.name}</option>)
-            }
-          </select>
-          <label htmlFor="dept">ساختمان : </label>
-          {/* dropdown for department */}
-          <select name="department" id="dept" value={this.state.department} onChange={(event) => this.onFormChange(event)}>
-            {
-              departments.map((department) => <option key={department.id} value={department.id}>{department.name}</option>)
-            }
-          </select>
-          {/* dropdown for priority */}
-          <label htmlFor="priority">اولویت</label>
-          <select name="priority" id="priority" value={this.state.priority} onChange={(event) => this.onFormChange(event)}>
-            <option value="HIGH">زیاد</option>
-            <option value="MEDIUM">متوسط</option>
-            <option value="LOW">کم</option>
-          </select>
+          <div className="input-group">
+            <label htmlFor="title">عنوان :</label>
+            <input placeholder="عنوان" type="text" name="title" value={this.state.title} onChange={(event) => this.onFormChange(event)} />
+          </div>
+          {/* dropdown for product */}
+          <div className="input-group">
+            <label htmlFor="product">محصول :</label>
+            <select name="product" id="product" value={this.state.product} onChange={(event) => this.onFormChange(event)} >
+              {
+                products.map((product) => <option key={product.id} value={product.id}>{product.name}</option>)
+              }
+            </select>
+          </div>
+          <div className="input-group">
+            <label htmlFor="dept">ساختمان :</label>
+            {/* dropdown for department */}
+            <select name="department" id="dept" value={this.state.department} onChange={(event) => this.onFormChange(event)}>
+              {
+                departments.map((department) => <option key={department.id} value={department.id}>{department.name}</option>)
+              }
+            </select>
+          </div>
+          <div className="input-group">
+            {/* dropdown for priority */}
+            <label htmlFor="priority">اولویت :</label>
+            <select name="priority" id="priority" value={this.state.priority} onChange={(event) => this.onFormChange(event)}>
+              <option value="HIGH">زیاد</option>
+              <option value="MEDIUM">متوسط</option>
+              <option value="LOW">کم</option>
+            </select>
+          </div>
+
+
+
           {/* textArea for description */}
           <textarea placeholder="توضیحات" name="description" cols="30" rows="10" value={this.state.description} onChange={(event) => this.onFormChange(event)}></textarea>
           <br></br>
           <button className="button submit" type="submit"> ثبت </button>
+          {this.state.successMessage && <p className="addTicket__message addTicket__message--success">{this.state.successMessage}</p>}
         </form>
-        {this.state.successMessage && <p className="addTicket__message addTicket__message--success">{this.state.successMessage}</p>}
       </React.Fragment>
     )
   }
