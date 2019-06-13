2ad2aac4c482dbd597ba286b541a2db4221730fb
Amir Ebrahimian <44034763+AmirEb@users.noreply.github.com>
2019-06-13 23:40:27 +0430

    Update AddSupportForm.js

--- src/component/SupportTeam/AddSupportForm.js
+++ src/component/SupportTeam/AddSupportForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {departments} from "../../mockData/departments";
 import {roles} from "../../mockData/roles";
 
-class AddProduct extends Component {
+class AddSupportUser extends Component {
     state = {
         id: '',
         role: 'سرپرست',
@@ -37,3 +37,62 @@ class AddProduct extends Component {
         const email = e.target.value
         this.setState({email})
     }
+    
+    
+        render() {
+        return (
+
+            <div >
+                <form >
+                    <div >
+                        <h2>
+                            افزودن پشتیبان
+                        </h2>
+                    </div>
+                    <div >{this.state.error ? <h4>{this.state.error}</h4> : true}</div>
+
+                    <div >
+                        <label> نام کامل: </label>
+                        <input type="text" value={this.state.name} onChange={this.handleNameChange}></input>
+
+                    </div>
+
+                    <div >
+                        <label>نقش:</label>
+                        <select value={this.state.category} onChange={this.handelRole}>
+                            {roles.map((role=>(<option key={role.id} value={role.name} >{role.name}</option>)))}
+                        </select>
+
+                    </div>
+
+                    <div >
+                        <label>دپارتمان:</label>
+                        <select value={this.state.category} onChange={this.handelDepartment}>
+                            {departments.map((dept=>(<option key={dept.id} value={dept.name} >{dept.name}</option>)))}
+                        </select>
+
+                    </div>
+
+
+                    <div >
+                        <label> شمارهءتلفن: </label>
+                        <input type="number" value={this.state.phone} onChange={this.handlePhoneChange}></input>
+
+                    </div>
+                    <div >
+                        <label> ایمیل: </label>
+                        <input type="email" value={this.state.email} onChange={this.handleEmailChange}></input>
+
+                    </div>
+
+                    <div >
+                        <button  onClick={this.handleSubmit}>ثبت پشتیبان</button>
+                    </div>
+                </form>
+            </div>
+
+        );
+    }
+}
+
+export default AddSupportUser;
