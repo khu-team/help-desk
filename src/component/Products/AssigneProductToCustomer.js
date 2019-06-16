4ff782e4cb95cb1cce3fd7059a3129457285dae0
morteza shojaei` <mshojaei179@gmail.com>
2019-06-16 12:26:53 +0430

    create add product with delete button.

--- src/component/Products/AssigneProductToCustomer.js
+++ src/component/Products/AssigneProductToCustomer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {products} from '../../mockData/products';
+import ProductsInAssigne from './ProductsInAssigne'
 class AssigneProductToCustomer extends Component {
     state = { 
         products:[],
@@ -10,9 +11,30 @@ class AssigneProductToCustomer extends Component {
 
      handelProductChange = (e)=>{
         const product = e.target.value;
-        this.setState({product})
+        this.setState({product});
      }
 
+     handleAddProduct=(e)=>{
+         e.preventDefault();
+         const stateproduct = this.state.product;
+         const tempproducts = this.state.products;
+         const temp_check_duplicated = tempproducts.filter(p =>p.name==stateproduct);
+         if(temp_check_duplicated.length===1){
+            alert("There is a product with this name!")
+         }
+         else{
+            const newproduct = products.filter(p=>p.name===stateproduct)[0];
+            tempproducts.push(newproduct);
+            this.setState({products:tempproducts})
+           }
+     }
+    
+    handleDeleteProduct = (e,product)=>{
+    e.preventDefault();
+    const prevproducts = this.state.products;
+    const products = prevproducts.filter(p=>p !==product);
+    this.setState({products});
+}
 
     render() { 
         return ( 
@@ -20,14 +42,14 @@ class AssigneProductToCustomer extends Component {
 <form>
     
 <div className="">       
-            <select value={this.state.product} onChange={this.handelProductChange}>
-                {products.map((products=>(<option key={products.id} value={products.name} >{products.name}</option>)))}
+            <select  value={this.state.product} onChange={this.handelProductChange}>
+                {products.map((product=>(<option  key={product.id} value={product.name} >{product.name}</option>)))}
             </select>
-            <label>محصول</label>
             </div>
 
+<ProductsInAssigne handleDeleteProduct={this.handleDeleteProduct} products={this.state.products}/>
 
-<button className="">Add</button>
+<button onClick={this.handleAddProduct} className="">Add</button>
 
 </form>
 
