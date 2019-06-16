b740237484351822f74ac56dbea01ec120b703f6
sajjad1999 <sajjadmashhadi7@gmail.com>
2019-06-16 14:50:32 +0430

    complete style for buttons

--- src/component/Products/ProductInAssigne.js
+++ src/component/Products/ProductInAssigne.js
@@ -2,7 +2,7 @@ import React from 'react'
 const ProductInReact = (props) => {
     return ( 
         <React.Fragment>
-        <li className=''>{props.product.name}<span><button className="" onClick={(e)=>props.handleDeleteProduct(e,props.product)}>Delete</button></span></li>
+        <li className=''>{props.product.name}<span><button className="assign-delete-button" onClick={(e)=>props.handleDeleteProduct(e,props.product)}>Delete</button></span></li>
         </React.Fragment>
      );
 }
