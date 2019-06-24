ff509d234f9d544e5c2b97ee504871f9886fd2fa
sajjad1999 <sajjadmashhadi7@gmail.com>
2019-06-24 18:58:13 +0430

    make AddProduct reusable

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import AssigneProductToCustomer from './Products/AssigneProductToCustomer';
+import EditProduct from './Edit/EditProduct';
 
 const Dashboard = () => (
     <div className="welcome">
         <AssigneProductToCustomer />
+        <EditProduct product={{
+            id: 88888,
+            platform: 'Android',
+            customerId: 55555,
+            categoryId: 3,
+            category: 'هاست و سرور',
+            ram: 8
+        }}/>
     </div>
 );
 
