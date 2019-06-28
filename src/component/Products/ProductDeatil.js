2a5117007174a07a3411d18540751e900e49b1d7
MehradNV <mehradnoorani@yahoo.com>
2019-06-28 22:37:25 +0430

    completed ProductDetail

--- src/component/Products/ProductDeatil.js
+++ src/component/Products/ProductDeatil.js
@@ -1,31 +1,61 @@
 import React from 'react';
 import {products} from "../../mockData/products";
+import {transactions} from "../../mockData/transactions";
 import '../../products.css';
 
 class ProductDetail extends React.Component {
 
     ShowDetail=()=> {
+        var numeral = require('numeral');
         const ID = this.props.location.state.ID
+        const ntransaction=transactions.filter((t)=>t.product==ID)
         const nproduct=products.find((p)=> p.id == ID)
-        const {id,name,price,category}=nproduct;
+        const {id,name,price,category,duration,discountPercentage}=nproduct;
 
         return(
-            <ul className='font-iran-sans'>
-                <li>آیدی:{id}</li>
-                <li>نام:{name}</li>
-                <li>قیمت:{price}</li>
-                <li>کتگوری:{category}</li>
-            </ul>
+            <div>
+              <table className='customers font-iran-sans'>
+                  <tbody>
+                  <tr>
+                      <th>آیدی محصول</th>
+                      <th>نام</th>
+                      {category==3 && <th>مدت زمان فعالیت</th>}
+                      <th>قیمت</th>
+                      <th>درصد تخفیف</th>
+                  </tr>
+                  <tr>
+                      <td>{id}</td>
+                      <td>{name}</td>
+                      {category==3 && <td>{duration}</td>}
+                      <td>{numeral(price).format('0,0')} تومان</td>
+                      <td>%{discountPercentage}</td>
+                  </tr>
+                  </tbody>
+              </table>
+
+                <table  className='customers font-iran-sans'>
+                    <tbody>
+                  <tr>
+                    <th>آیدی خریداران</th>
+                    <th>تاریخ انقضا</th>
+                  </tr>
+                {ntransaction.map((t)=>{
+                    return(
+                        <tr>
+                            <td>{t.costumer}</td>
+                            <td>{t.expireDate.calendar()}</td>
+                        </tr>
+                    )
+                })}
+                  </tbody>
+                </table>
+            </div>
         )
     }
 
 
     render() {
-        return (
-            <div>
-                {this.ShowDetail()}
-            </div>
-        );
+        return this.ShowDetail()
     }
 }
 
