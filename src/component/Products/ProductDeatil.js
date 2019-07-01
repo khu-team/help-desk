ac691e1c1938cc78094fb272e440be41d249dbbb
maryam-Si <49492170+maryam-Si@users.noreply.github.com>
2019-07-01 13:18:26 +0430

    Revert "debug styling!!"
    This reverts commit 44b71ece8851d9d1f9fbc044bba4ca1aefb9e547.

--- src/component/Products/ProductDeatil.js
+++ src/component/Products/ProductDeatil.js
@@ -1,85 +1,57 @@
 import React from 'react';
 import {products} from "../../mockData/products";
 import {transactions} from "../../mockData/transactions";
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import {costumers} from "../../mockData/costumers";
 import '../../products.css';
 
 class ProductDetail extends React.Component {
 
     ShowDetail=()=> {
         var numeral = require('numeral');
-        const ID = this.props.location.state.ID
-        const ntransaction=transactions.filter((t)=>t.product==ID)
-        const nproduct=products.find((p)=> p.id == ID)
+        const ID = this.props.location.state.ID;
+        const ntransaction=transactions.filter((t)=>t.product==ID);
+        const nproduct=products.find((p)=> p.id == ID);
         const {id,name,price,category,duration,discountPercentage}=nproduct;
 
         return(
             <div>
-            <Paper >
-          <Table className='sall'>
-          <TableHead >
-        <TableRow  className='headstyle'>
-          <TableCell className='cell'>آیدی محصول</TableCell>
-          <TableCell className='cell' align="right">نام </TableCell>
-         {category==3 &&<TableCell className='cell' align="right">مدت زمان فعالیت</TableCell>} 
-          <TableCell className='cell' align="right">قیمت</TableCell>
-          <TableCell className='cell' onClick={this.Sort}   align="right">درصد تخفیف </TableCell>
-         
+              <table className='customers font-iran-sans'>
+                  <tbody>
+                  <tr>
+                      <th>آیدی محصول</th>
+                      <th>نام</th>
+                      {category==3 && <th>مدت زمان فعالیت</th>}
+                      <th>قیمت</th>
+                      <th>درصد تخفیف</th>
+                  </tr>
+                  <tr>
+                      <td>{id}</td>
+                      <td>{name}</td>
+                      {category==3 && <td>{duration}</td>}
+                      <td>{numeral(price).format('0,0')} تومان</td>
+                      <td>%{discountPercentage}</td>
+                  </tr>
+                  </tbody>
+              </table>
 
-        </TableRow>
-      </TableHead>
-
-            <TableBody>
-            
-              <TableRow >
-               
-                <TableCell align="right">{id}</TableCell>
-                <TableCell align="right">{name}</TableCell>
-                {category==3 &&<TableCell align="right">{duration}</TableCell>}
-                <TableCell align="right">{numeral(price).format('0,0')}تومان </TableCell>
-                <TableCell align="right">%{discountPercentage}</TableCell>
-
-              </TableRow>
-          
-            </TableBody>
-          </Table>
-        </Paper>  
-
-
-
-        <Paper >
-          <Table className='sall'>
-          <TableHead >
-        <TableRow  className='headstyle'>
-          <TableCell className='cell'>آیدی خریداران</TableCell>
-          <TableCell className='cell' align="right">تاریخ انقضا </TableCell>
-       
-        </TableRow>
-      </TableHead>
-
-            <TableBody>
-            
-            {ntransaction.map((t)=>{
+                <table  className='customers font-iran-sans'>
+                    <tbody>
+                  <tr>
+                    <th>آیدی خریدار</th>
+                    <th>نام خریدار</th>
+                    <th>تاریخ انقضا</th>
+                  </tr>
+                {ntransaction.map((t)=>{
                     return(
-                        <TableRow>
-                            <TableCell>{t.costumer}</TableCell>
-                            <TableCell>{t.expireDate.calendar()}</TableCell>
-                        </TableRow>
+                        <tr>
+                            <td>{t.costumer}</td>
+                            <td>{costumers.find((c)=>c.id==t.costumer).fullName}</td>
+                            <td>{t.expireDate.calendar()}</td>
+                        </tr>
                     )
                 })}
-          
-            </TableBody>
-          </Table>
-        </Paper>  
-
-
-
+                  </tbody>
+                </table>
             </div>
         )
     }
@@ -87,7 +59,6 @@ class ProductDetail extends React.Component {
 
     render() {
         return this.ShowDetail()
-
     }
 }
 
