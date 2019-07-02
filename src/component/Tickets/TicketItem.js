610a1cd1710f82574a465a6e0251862b51f8a0f1
Aminsaffarzadeh <49522244+Aminsaffarzadeh@users.noreply.github.com>
2019-07-02 13:29:29 +0430

    changing table

--- src/component/Tickets/TicketItem.js
+++ src/component/Tickets/TicketItem.js
@@ -11,15 +11,15 @@ const TicketItem = (props) => (
 		<td className="column1 font-iran-sans">{props.ticket.id}</td>
 		<td className="column2 font-iran-sans">{props.ticket.title}</td>
 		<td className="column3 font-iran-sans">
-			{props.ticket.priority === "HIGH" && <Chip className="hchip" label="HIGH"/>}
-			{props.ticket.priority === "MEDIUM" && <Chip className="mchip" label="MEDIUM"/>}
-			{props.ticket.priority === "LOW" && <Chip className="lchip" label="LOW"/>}
+			{props.ticket.priority === "HIGH" && <Chip className="hchip" label="زیاد"/>}
+			{props.ticket.priority === "MEDIUM" && <Chip className="mchip" label="متوسط"/>}
+			{props.ticket.priority === "LOW" && <Chip className="lchip" label="کم"/>}
 		</td>
 		<CostumerRenderer costumerId={props.ticket.costumer} />
 		<ProductRenderer productId={props.ticket.product} />
 		<DepartmentRenderer departmentId={props.ticket.department} />
 		<td className="column7 font-iran-sans">
-			{props.ticket.answerStatus ? <Chip label="True" color='primary'/> : <Chip label="False"  color="secondary" />}
+			{props.ticket.answerStatus ? <Chip label="درست" color='primary'/> : <Chip label="غلط"  color="secondary" />}
 		</td>
 		<td className="column82 font-iran-sans">{props.ticket.submissionDateTime}</td>
 		<td className="column9 font-iran-sans"> <NavLink to = {`/ticket/${props.ticket.id}`}>بیشتر</NavLink> </td>
