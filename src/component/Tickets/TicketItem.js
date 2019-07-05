55b880b66bed7e59f9ba72880c9402e1250191d9
Aminsaffarzadeh <49522244+Aminsaffarzadeh@users.noreply.github.com>
2019-07-05 22:04:16 +0430

    fixing problems

--- src/component/Tickets/TicketItem.js
+++ src/component/Tickets/TicketItem.js
@@ -11,15 +11,15 @@ const TicketItem = (props) => (
 		<td className="column1 font-iran-sans">{props.ticket.id}</td>
 		<td className="column2 font-iran-sans">{props.ticket.title}</td>
 		<td className="column3 font-iran-sans">
-			{props.ticket.priority === "HIGH" && <Chip className="hchip" label="زیاد"/>}
-			{props.ticket.priority === "MEDIUM" && <Chip className="mchip" label="متوسط"/>}
-			{props.ticket.priority === "LOW" && <Chip className="lchip" label="کم"/>}
+			{props.ticket.priority === "HIGH" && <Chip className="hchip font-iran-sans" label="زیاد"/>}
+			{props.ticket.priority === "MEDIUM" && <Chip className="mchip font-iran-sans" label="متوسط"/>}
+			{props.ticket.priority === "LOW" && <Chip className="lchip font-iran-sans" label="کم"/>}
 		</td>
 		<CostumerRenderer costumerId={props.ticket.costumer} />
 		<ProductRenderer productId={props.ticket.product} />
 		<DepartmentRenderer departmentId={props.ticket.department} />
 		<td className="column7 font-iran-sans">
-			{props.ticket.answerStatus ? <Chip label="درست" color='primary'/> : <Chip label="غلط"  color="secondary" />}
+			{props.ticket.answerStatus ? <Chip className="font-iran-sans" label="پاسخ داده شده" color='primary'/> : <Chip className="font-iran-sans" label="پاسخ داده نشده"  color="secondary" />}
 		</td>
 		<td className="column82 font-iran-sans">{props.ticket.submissionDateTime}</td>
 		<td className="column9 font-iran-sans"> <NavLink to = {`/ticket/${props.ticket.id}`}>بیشتر</NavLink> </td>
